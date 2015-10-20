cb0f825db46e6942bd1c449abdd7a84d8ed7e2df
dudegrim <cardanomarcdaniel96@gmail.com>
2015-10-20 19:14:04 +0800

    Added the cartesian plane.

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -7,16 +7,58 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 import com.sun.istack.internal.FragmentContentHandler;
+import javafx.application.Platform;
+import com.sun.xml.internal.bind.v2.runtime.output.IndentingUTF8XmlOutput;
 
+import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 @SuppressWarnings("serial")
 public class MainView extends JFrame {
+	private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+	
+	private static Scene createScene() {
+		Group  root  =  new  Group();
+        Axes axes = new Axes(
+	            300, 300,
+	            -8, 8, 1,
+	            -6, 6, 1
+	    );
+	    
+	    Plot plot = new Plot(
+	            //x -> -x*x/4,// parabola function
+	    		//x -> Math.pow(4*x, 0.5),
+	    		x -> -x*x/4,
+	    		-8, 8, 0.1,
+	            axes
+	    );
+	
+	    StackPane layout = new StackPane(plot);
+	    layout.setPadding(new Insets(20));
+	    layout.setStyle("-fx-background-color: rgb(35, 39, 50);");
+	    Scene  theScene  =  new Scene(layout, Color.rgb(35, 39, 50));
+	    return theScene;
+	}
+
 	private JFrame mainFrame = new JFrame();
 	private JPanel inputsPanel =  new JPanel();
 	private JPanel controlPanel = new JPanel();
 	private JPanel operationPanel = new JPanel();
-	private JPanel planePanel = new JPanel();
+	private JFXPanel planePane = new JFXPanel();
+	
+	private JPanel planePanel;
+	
 	
 	String[] types = {"Points, Lines", "Polygons", "Conics"};
 	String[] conics = {"Cirlce", "Ellipse", "Parabola", "Hyperbola"};
@@ -27,7 +69,7 @@ public class MainView extends JFrame {
 	private BorderLayout inputLayout = new BorderLayout();
 	private GridBagLayout frameLayout = new GridBagLayout();
 	private GridLayout controlsLayout = new GridLayout(2,1);
-	Border blackline = BorderFactory.createLineBorder(Color.black);
+	
 	
 	private PointsAndLines pal = new PointsAndLines(); 
 	private Polygons poly = new Polygons();
@@ -39,25 +81,38 @@ public class MainView extends JFrame {
 		inputsPanel.setLayout(inputLayout);
 		inputsPanel.add(typesCombo, BorderLayout.PAGE_START);
 		
+		
+		 
+		Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(planePane);
+            }
+       });
+		
+		
+		
 		//setting mainFrame Components
 		JSplitPane split = new JSplitPane();
-		split.add(planePanel, JSplitPane.LEFT);
+		split.add(planePane, JSplitPane.LEFT);
 		split.add(controlPanel, JSplitPane.RIGHT);
 		split.setOneTouchExpandable(true);
 		split.setDividerSize(10);
 		split.setDividerLocation(700);
 		mainFrame.add(split);
 		
-		planePanel.setBorder(blackline);
+	//	planePanel.setBorder(blackline);
 		//TODO  planePanel.add();
 		
 		//setting up controlPanel
-		controlPanel.setBorder(blackline);
+	//	controlPanel.setBorder(blackline);
 		controlPanel.setLayout(controlsLayout);
-		inputsPanel.setBorder(blackline);
-		operationPanel.setBorder(blackline);
+	//	inputsPanel.setBorder(blackline);
+	//	operationPanel.setBorder(blackline);
 		controlPanel.add(inputsPanel);
 		controlPanel.add(operationPanel);
+		
+		
 		
 		
 		mainFrame.setResizable(true);
@@ -88,3 +143,5 @@ public class MainView extends JFrame {
 		}
 	}
 }
+
+//NumberAxis Source: http://stackoverflow.com/questions/24005247/draw-cartesian-plane-graphi-with-canvas-in-javafx
